dd76a56c2252a86b0b165a826bee8c9beba8dc86
MIKE LE MINH NGUYEN <minhnl@yahoo.com>
2022-04-02 21:35:38 -0400

    TS classes

--- classes.ts
+++ classes.ts
@@ -1,16 +1,30 @@
 import { hasUncaughtExceptionCaptureCallback } from 'process';
 
 class Vehicle {
-  drive(): void {
-    console.log('chugga chugga');
-  }
-  honk(): void {
+  constructor(public color: string) {}
+
+  protected honk(): void {
     console.log('beep');
   }
 }
 
-class Car extends Vehicle {}
+const vehicle = new Vehicle('orange');
+console.log(vehicle.color);
 
-const vehicle = new Vehicle();
-vehicle.drive();
-vehicle.honk();
+class Car extends Vehicle {
+  constructor(public wheels: number, color: string) {
+    super(color);
+  }
+
+  private drive(): void {
+    console.log('vroom');
+  }
+
+  startDrivingProcess(): void {
+    this.drive();
+    this.honk();
+  }
+}
+
+const car = new Car(4, 'red');
+car.startDrivingProcess();
